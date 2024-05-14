4b96a1c172880904c99cd0cf215d10fe1707d7ad
João Carlos <joaocarlos.cic1@gmail.com>
2024-05-13 23:02:02 -0300

    Initial Commit

---
+++ src/main/java/br/com/personalgymapi/PersonalGymApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.personalgymapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalGymApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalGymApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/personalgymapi/PersonalGymApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.personalgymapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalGymApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
