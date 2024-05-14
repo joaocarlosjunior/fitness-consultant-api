71cad17ead11183a65d2c7c949e59123ef353ff5
João Carlos <joaocarlos.cic1@gmail.com>
2024-05-13 23:13:01 -0300

    feat: added repository User and Training

---
+++ src/main/java/br/com/personalgymapi/domain/repository/TrainingRepository.java
@@ -0,0 +1,7 @@
+package br.com.personalgymapi.domain.repository;
+
+import br.com.personalgymapi.domain.entities.Training;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrainingRepository extends JpaRepository<Training,Integer> {
+}
