ca8e4e986113f91ad8dfdec1fa73f5fe6257af97
João Carlos <joaocarlos.cic1@gmail.com>
2024-05-16 18:03:24 -0300

    refactor: removed imports

--- src/main/java/br/com/personalgymapi/dto/user/RegisterUserDTO.java
+++ src/main/java/br/com/personalgymapi/dto/user/RegisterUserDTO.java
@@ -1,7 +1,6 @@
 package br.com.personalgymapi.dto.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
