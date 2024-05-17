5712bcdb3122521ee93f91201737159cbb26cd2d
João Carlos <joaocarlos.cic1@gmail.com>
2024-05-16 22:45:42 -0300

    feat: added validators Pattern and Email

--- src/main/java/br/com/personalgymapi/dto/user/RegisterUserDTO.java
+++ src/main/java/br/com/personalgymapi/dto/user/RegisterUserDTO.java
@@ -1,28 +1,33 @@
 package br.com.personalgymapi.dto.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class RegisterUserDTO {
-    @NotBlank(message = "Campo Nome Obrigatório")
     @JsonProperty("first_name")
+    @NotBlank(message = "Campo Nome Obrigatório")
+    @Pattern(regexp = "^\\p{L}+$", message = "O campo Nome deve conter apenas letras")
     private String firstName;
 
-    @NotBlank(message = "Campo Sobrenome Obrigatório")
     @JsonProperty("last_name")
+    @NotBlank(message = "Campo Sobrenome Obrigatório")
+    @Pattern(regexp = "^\\p{L}+$", message = "O campo Sobrenome deve conter apenas letras")
     private String lastName;
 
-    @NotBlank(message = "Campo email Obrigatório")
     @JsonProperty("email")
+    @NotBlank(message = "Campo email Obrigatório")
+    @Email
     private String email;
 
-    @NotBlank(message = "Campo Telefone Obrigatório")
     @JsonProperty("phone")
+    @NotBlank(message = "Campo Telefone Obrigatório")
     private String phone;
 
-    @NotBlank(message = "Campo Sobrenome Obrigatório")
     @JsonProperty("password")
+    @NotBlank(message = "Campo Sobrenome Obrigatório")
     private String password;
 }
