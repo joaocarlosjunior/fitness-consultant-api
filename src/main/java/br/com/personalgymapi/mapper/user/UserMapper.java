d630f9dc9fd793b3b4739116e4332750e8acf51e
João Carlos <joaocarlos.cic1@gmail.com>
2024-05-16 22:47:31 -0300

    refactor: renamed method to toEntity

--- src/main/java/br/com/personalgymapi/mapper/user/UserMapper.java
+++ src/main/java/br/com/personalgymapi/mapper/user/UserMapper.java
@@ -2,11 +2,13 @@ package br.com.personalgymapi.mapper.user;
 
 import br.com.personalgymapi.domain.entities.User;
 import br.com.personalgymapi.dto.user.UpdateUserDTO;
-import org.mapstruct.*;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING,
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-public interface UserMapper {
-    void updateDtoFromUser(UpdateUserDTO registerUserDTO, @MappingTarget User user);
-
+public interface UserMapper{
+    void toEntity(UpdateUserDTO dto, @MappingTarget User entity);
 }
