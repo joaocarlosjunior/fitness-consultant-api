0a810939c7d85f982f87121d0b20701a707cf046
João Carlos <joaocarlos.cic1@gmail.com>
2024-05-16 22:46:32 -0300

    feat: added validators Pattern and Email

--- src/main/java/br/com/personalgymapi/dto/user/UpdateUserDTO.java
+++ src/main/java/br/com/personalgymapi/dto/user/UpdateUserDTO.java
@@ -2,18 +2,23 @@ package br.com.personalgymapi.dto.user;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UpdateUserDTO {
     @JsonProperty("first_name")
+    @Pattern(regexp = "^\\p{L}+$", message = "O campo Sobrenome deve conter apenas letras")
     private String firstName;
 
     @JsonProperty("last_name")
+    @Pattern(regexp = "^\\p{L}+$", message = "O campo Sobrenome deve conter apenas letras")
     private String lastName;
 
     @JsonProperty("email")
+    @Email
     private String email;
 
     @JsonProperty("phone")
