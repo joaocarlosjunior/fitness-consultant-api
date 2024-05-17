d0903aaef8ec8cc61bd211e4aa7e3c3124464431
João Carlos <joaocarlos.cic1@gmail.com>
2024-05-17 09:52:08 -0300

    feat: created query method search by muscle group name

--- src/main/java/br/com/personalgymapi/domain/repository/MuscleGroupRepository.java
+++ src/main/java/br/com/personalgymapi/domain/repository/MuscleGroupRepository.java
@@ -4,6 +4,9 @@ import br.com.personalgymapi.domain.entities.MuscleGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MuscleGroupRepository extends JpaRepository<MuscleGroup,Long> {
+    Optional<MuscleGroup> findByName(String name);
 }
