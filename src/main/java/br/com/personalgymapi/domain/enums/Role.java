2712ebc005af4927c57f5d0b98ab71cb1a516cb4
João Carlos <joaocarlos.cic1@gmail.com>
2024-05-20 18:31:24 -0300

    chore: update enum Role

--- src/main/java/br/com/personalgymapi/domain/enums/Role.java
+++ src/main/java/br/com/personalgymapi/domain/enums/Role.java
@@ -1,9 +1,30 @@
 package br.com.personalgymapi.domain.enums;
 
+import lombok.Getter;
+
+import java.util.HashMap;
+
+@Getter
 public enum Role {
     ROLE_USER(0),
     ROLE_ADMIN(1);
 
-    Role(int i) {
+    private final int value;
+
+    Role(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public static Role fromValue(int value) {
+        for (Role role : Role.values()) {
+            if (role.getValue() == value) {
+                return role;
+            }
+        }
+        throw new IllegalArgumentException("Valor de Role inválido: " + value);
     }
 }
