f187da3cd677a96f33100f8ef5c7857a37ab7fed
João Carlos <joaocarlos.cic1@gmail.com>
2024-05-20 09:40:42 -0300

    update file V1 migration

--- src/main/resources/db/migration/V1__init_database.sql
+++ src/main/resources/db/migration/V1__init_database.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS tbl_user (
     id_user BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
-    first_name varchar(15) NOT NULL,
-    last_name varchar(20) NOT NULL,
-    email varchar(50) NOT NULL,
-    phone varchar(20) NOT NULL,
-    password varchar,
-    is_active boolean NOT NULL,
-    role_user varchar(20) NOT NULL,
-    created_at timestamp,
-    updated_at timestamp,
+    first_name VARCHAR(15) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    password VARCHAR,
+    is_active BOOLEAN NOT NULL,
+    role_user VARCHAR(20) NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
     PRIMARY KEY (id_user),
     CONSTRAINT email_unique UNIQUE (email),
     CONSTRAINT phone_unique UNIQUE (phone),
@@ -17,10 +17,10 @@ CREATE TABLE IF NOT EXISTS tbl_user (
 
 CREATE TABLE IF NOT EXISTS tbl_training (
     id_training BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
-    training_type varchar(8),
-    is_done boolean,
-    created_at timestamp,
-    updated_at timestamp,
+    training_type VARCHAR(8),
+    is_done BOOLEAN,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
     id_user BIGINT,
     PRIMARY KEY (id_training),
     CONSTRAINT training_type_unique UNIQUE (training_type),
@@ -29,8 +29,8 @@ CREATE TABLE IF NOT EXISTS tbl_training (
 
 CREATE TABLE IF NOT EXISTS tbl_training_time (
     id_training_time BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
-    start_time timestamp,
-    final_time timestamp,
+    start_time TIMESTAMP,
+    final_time TIMESTAMP,
     training_date date,
     id_training BIGINT,
     PRIMARY KEY (id_training_time),
@@ -39,7 +39,7 @@ CREATE TABLE IF NOT EXISTS tbl_training_time (
 
 CREATE TABLE IF NOT EXISTS tbl_muscle_group (
     id_muscle_group BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
-    name_group varchar(50),
+    name_group VARCHAR(50),
     PRIMARY KEY (id_muscle_group)
 );
 
@@ -50,18 +50,18 @@ CREATE TABLE IF NOT EXISTS tbl_musclegroup_training (
 
 CREATE TABLE IF NOT EXISTS tbl_exercise_name (
     id_exercise_name BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
-    exercise_name varchar(30) NOT NULL,
+    exercise_name VARCHAR(30) NOT NULL,
     CONSTRAINT exercise_name_unique UNIQUE (exercise_name),
     PRIMARY KEY (id_exercise_name)
 );
 
 CREATE TABLE IF NOT EXISTS tbl_heating (
     id_heating BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
-    series smallint,
-    repetitions varchar(10),
-    initial_load smallint,
-    final_load smallint,
-    method_exercise varchar(30),
+    series SMALLINT,
+    repetitions VARCHAR(10),
+    initial_load SMALLINT,
+    final_load SMALLINT,
+    method_exercise VARCHAR(30),
     id_training BIGINT,
     id_exercise_name BIGINT,
     PRIMARY KEY (id_heating),
@@ -71,11 +71,11 @@ CREATE TABLE IF NOT EXISTS tbl_heating (
 
 CREATE TABLE IF NOT EXISTS tbl_exercise (
     id_exercise BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
-    series smallint,
-    repetitions varchar(10),
-    initial_load smallint,
-    final_load smallint,
-    method_exercise varchar(30),
+    series SMALLINT,
+    repetitions VARCHAR(10),
+    initial_load SMALLINT,
+    final_load SMALLINT,
+    method_exercise VARCHAR(30),
     id_training BIGINT,
     id_exercise_name BIGINT,
     PRIMARY KEY (id_exercise),
