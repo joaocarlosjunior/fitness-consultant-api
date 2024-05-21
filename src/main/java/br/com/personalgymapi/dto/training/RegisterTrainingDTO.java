f6072a7761297ec66fc29f0a6a60b9577131c534
João Carlos <joaocarlos.cic1@gmail.com>
2024-05-21 17:52:48 -0300

    feat: added set muscle group

--- src/main/java/br/com/personalgymapi/dto/training/RegisterTrainingDTO.java
+++ src/main/java/br/com/personalgymapi/dto/training/RegisterTrainingDTO.java
@@ -1,9 +1,23 @@
 package br.com.personalgymapi.dto.training;
 
+import br.com.personalgymapi.dto.musuculegroup.InfoMuscleGroupDTO;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+
+import java.util.Set;
 
 @Data
 public class RegisterTrainingDTO {
-    private Long id_user;
-    private String training_type;
+    @JsonProperty("id_user")
+    @NotNull(message = "Campo id user não pode ser nulo")
+    private Long idUser;
+
+    @JsonProperty("training_type")
+    @NotNull(message = "Campo training_type não pode ser nulo")
+    private Integer trainingType;
+
+    @JsonProperty("muscle_groups")
+    @NotNull(message = "Campo Grupo Muscular obrigatório")
+    private Set<InfoMuscleGroupDTO> muscleGroups;
 }
