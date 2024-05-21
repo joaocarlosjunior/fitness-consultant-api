f1cee7bd7a597efb3f9f222117581e8af798a4be
João Carlos <joaocarlos.cic1@gmail.com>
2024-05-21 17:53:27 -0300

    feat: added standardized message in the fields

--- src/main/java/br/com/personalgymapi/dto/user/RegisterUserDTO.java
+++ src/main/java/br/com/personalgymapi/dto/user/RegisterUserDTO.java
@@ -3,33 +3,32 @@ package br.com.personalgymapi.dto.user;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class RegisterUserDTO {
     @JsonProperty("first_name")
-    @NotBlank(message = "{campo.user.first-name}")
+    @NotBlank(message = "{field.user.first-name}")
     @Pattern(regexp = "^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ' ]+$", message = "O campo Nome deve conter apenas letras")
     private String firstName;
 
     @JsonProperty("last_name")
-    @NotBlank(message = "{campo.user.last-name}")
+    @NotBlank(message = "{field.user.last-name}")
     @Pattern(regexp = "^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ' ]+$", message = "O campo Sobrenome deve conter apenas letras")
     private String lastName;
 
     @JsonProperty("email")
-    @NotBlank(message = "{campo.user.email}")
+    @NotBlank(message = "{field.user.email}")
     @Email
     private String email;
 
     @JsonProperty("phone")
-    @NotBlank(message = "{campo.user.phone}")
+    @NotBlank(message = "{field.user.phone}")
     private String phone;
 
     @JsonProperty("password")
-    @NotBlank(message = "{campo.user.senha}")
+    @NotBlank(message = "{field.user.password}")
     private String password;
 
     @JsonProperty("role")
