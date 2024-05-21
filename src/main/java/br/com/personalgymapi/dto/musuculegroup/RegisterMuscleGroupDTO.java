bd3a68d864266430fc6f958893b9d9287c6776f4
João Carlos <joaocarlos.cic1@gmail.com>
2024-05-21 17:50:54 -0300

    feat: added pattern validator

--- src/main/java/br/com/personalgymapi/dto/musuculegroup/RegisterMuscleGroupDTO.java
+++ src/main/java/br/com/personalgymapi/dto/musuculegroup/RegisterMuscleGroupDTO.java
@@ -1,10 +1,12 @@
 package br.com.personalgymapi.dto.musuculegroup;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class RegisterMuscleGroupDTO {
-    @NotBlank(message = "Campo Nome Grupo Muscular obrigatório")
+    @NotBlank(message = "{field.groupmuscle.name}")
+    @Pattern(regexp = "^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ' ]+$", message = "O campo Nome Grupo Muscular deve conter apenas letras")
     private String name;
 }
