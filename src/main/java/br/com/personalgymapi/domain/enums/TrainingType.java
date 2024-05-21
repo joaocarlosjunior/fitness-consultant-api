cc16586a647e3e06c9847b643a0be98e4336f114
João Carlos <joaocarlos.cic1@gmail.com>
2024-05-21 17:55:14 -0300

    refactor: refactored enum Training type

--- src/main/java/br/com/personalgymapi/domain/enums/TrainingType.java
+++ src/main/java/br/com/personalgymapi/domain/enums/TrainingType.java
@@ -1,9 +1,28 @@
 package br.com.personalgymapi.domain.enums;
 
 public enum TrainingType {
-    TREINO_A,
-    TREINO_B,
-    TREINO_C,
-    TREINO_D,
-    TREINO_E
+    A(0),
+    B(1),
+    C(2),
+    D(3),
+    E(4);
+
+    private final int value;
+
+    TrainingType(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public static TrainingType fromValue(int value) {
+        for (TrainingType role : TrainingType.values()) {
+            if (role.getValue() == value) {
+                return role;
+            }
+        }
+        throw new IllegalArgumentException("Valor de TrainingType inválido: " + value);
+    }
 }
