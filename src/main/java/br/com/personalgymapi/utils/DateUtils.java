9e17015b775a27e88e57c3773faec1cafa24f396
João Carlos <joaocarlos.cic1@gmail.com>
2024-05-23 16:36:07 -0300

    feat: created date utils

---
+++ src/main/java/br/com/personalgymapi/utils/DateUtils.java
@@ -0,0 +1,16 @@
+package br.com.personalgymapi.utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateUtils {
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+    public static String checkUpdateDate(LocalDateTime date) {
+        return date != null ? formatDate(date) : null;
+    }
+
+    public static String formatDate(LocalDateTime date) {
+        return date.format(FORMATTER);
+    }
+}
