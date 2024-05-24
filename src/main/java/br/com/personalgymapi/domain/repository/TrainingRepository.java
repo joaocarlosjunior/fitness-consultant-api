a2aa1b3b7bb5ab6cc78947a8b802d52863e13e43
João Carlos <joaocarlos.cic1@gmail.com>
2024-05-24 15:35:55 -0300

    chore: changed from Integer to Long primary key

--- src/main/java/br/com/personalgymapi/domain/repository/TrainingRepository.java
+++ src/main/java/br/com/personalgymapi/domain/repository/TrainingRepository.java
@@ -3,5 +3,5 @@ package br.com.personalgymapi.domain.repository;
 import br.com.personalgymapi.domain.entities.Training;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TrainingRepository extends JpaRepository<Training,Integer> {
+public interface TrainingRepository extends JpaRepository<Training,Long> {
 }
