cec9f66018bc488e0f0933ba450be42f668aa116
João Carlos <joaocarlos.cic1@gmail.com>
2024-05-24 15:36:57 -0300

    feat: added query method to email and phone

--- src/main/java/br/com/personalgymapi/domain/repository/UserRepository.java
+++ src/main/java/br/com/personalgymapi/domain/repository/UserRepository.java
@@ -4,12 +4,10 @@ import br.com.personalgymapi.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByEmail(String email);
+    boolean existsByEmailIgnoreCase(String email);
 
-    Optional<User> findByPhone(String phone);
+    boolean existsByPhone(String phone);
 }
