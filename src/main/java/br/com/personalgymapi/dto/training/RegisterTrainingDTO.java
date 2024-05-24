f8d0687e9007d0fe5fc9688cb15f639e4c7fdcce
João Carlos <joaocarlos.cic1@gmail.com>
2024-05-24 15:32:14 -0300

    remove/feat: removed muscle groups and added periodization

--- src/main/java/br/com/personalgymapi/dto/training/RegisterTrainingDTO.java
+++ src/main/java/br/com/personalgymapi/dto/training/RegisterTrainingDTO.java
@@ -1,23 +1,19 @@
 package br.com.personalgymapi.dto.training;
 
-import br.com.personalgymapi.dto.musuculegroup.InfoMuscleGroupDTO;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
-import java.util.Set;
-
 @Data
 public class RegisterTrainingDTO {
-    @JsonProperty("id_user")
+    @JsonProperty("id_periodization")
     @NotNull(message = "Campo id user não pode ser nulo")
-    private Long idUser;
+    private Long idPeriodization;
 
     @JsonProperty("training_type")
     @NotNull(message = "Campo training_type não pode ser nulo")
     private Integer trainingType;
 
-    @JsonProperty("muscle_groups")
-    @NotNull(message = "Campo Grupo Muscular obrigatório")
-    private Set<InfoMuscleGroupDTO> muscleGroups;
+    @JsonProperty("training_name")
+    private String trainingName;
 }
