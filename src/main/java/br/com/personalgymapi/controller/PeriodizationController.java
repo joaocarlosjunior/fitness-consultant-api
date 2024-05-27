c59030e1ce38dae7cb04ea28955a8d9795ec6d28
João Carlos <joaocarlos.cic1@gmail.com>
2024-05-26 21:24:32 -0300

    refactor: changed endpoint url

--- src/main/java/br/com/personalgymapi/controller/PeriodizationController.java
+++ src/main/java/br/com/personalgymapi/controller/PeriodizationController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/periodization")
+@RequestMapping("/api/v1/periodizations")
 @RequiredArgsConstructor
 public class PeriodizationController {
     private final PeriodizationService periodizationService;
