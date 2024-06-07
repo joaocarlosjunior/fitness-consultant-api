d1cf55bd0c57a9257d4467634d942e6ba7606a62
João Carlos <joaocarlos.cic1@gmail.com>
2024-06-07 12:09:22 -0300

    refactor: removed date hours

--- src/main/java/br/com/personalgymapi/utils/DateUtils.java
+++ src/main/java/br/com/personalgymapi/utils/DateUtils.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateUtils {
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     public static String checkUpdateDate(LocalDateTime date) {
         return date != null ? formatDate(date) : null;
