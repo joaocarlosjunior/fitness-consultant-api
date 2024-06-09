4de363538c2abe8763c168b23c0fa00c9884d88d
João Carlos <joaocarlos.cic1@gmail.com>
2024-06-09 20:20:01 -0300

    feat: added query methods delete by id user

--- src/main/java/br/com/fitnessconsultant/domain/repository/ExerciseRepository.java
+++ src/main/java/br/com/fitnessconsultant/domain/repository/ExerciseRepository.java
@@ -3,7 +3,9 @@ package br.com.fitnessconsultant.domain.repository;
 import br.com.fitnessconsultant.domain.entities.Exercise;
 import jakarta.websocket.server.PathParam;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +14,17 @@ import java.util.List;
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
     @Query(value = "select e from Exercise e where e.training.id=:id")
     List<Exercise> getAllExercisesByIdTraining(@PathParam("id") Long id);
+
+    @Query("""
+        delete from Exercise e
+        where e.training.id in (
+            select t.id from Training t
+            where t.periodization.id in (
+                select p.id from Periodization p
+                where p.user.id = :userId
+            )
+        )
+        """)
+    @Modifying
+    void deleteAllByUserId(@Param("userId") Long userId);
 }
--- src/main/java/br/com/fitnessconsultant/domain/repository/TrainingRepository.java
+++ src/main/java/br/com/fitnessconsultant/domain/repository/TrainingRepository.java
@@ -3,11 +3,23 @@ package br.com.fitnessconsultant.domain.repository;
 import br.com.fitnessconsultant.domain.entities.Training;
 import jakarta.websocket.server.PathParam;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface TrainingRepository extends JpaRepository<Training,Long> {
     @Query(value = "select t from Training t where t.periodization.id=:id")
     List<Training> getAllTrainingByIdPeriodization(@PathParam("id") Long id);
+
+    @Query("""
+        delete from Training t
+        where t.periodization.id in (
+            select p.id from Periodization p
+            where p.user.id = :userId
+        )
+        """)
+    @Modifying
+    void deleteAllByUserId(@Param("userId") Long userId);
 }
