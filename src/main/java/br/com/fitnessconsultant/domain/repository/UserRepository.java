12154a1caa97caaff3d81a28780a23bc435b35b6
João Carlos <joaocarlos.cic1@gmail.com>
2024-06-09 20:20:56 -0300

    feat: added query method findAllUserIsActive

--- src/main/java/br/com/fitnessconsultant/domain/repository/UserRepository.java
+++ src/main/java/br/com/fitnessconsultant/domain/repository/UserRepository.java
@@ -2,12 +2,20 @@ package br.com.fitnessconsultant.domain.repository;
 
 import br.com.fitnessconsultant.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, CustomizedUserRepository<Long> {
 
     boolean existsByEmailIgnoreCase(String email);
 
     boolean existsByPhone(String phone);
+
+    @Query("select u from User u where u.isActive=:isActive")
+    List<User> findAllUserIsActive(@Param("isActive") boolean isActive);
+
 }
