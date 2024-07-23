a55b2406fcf90d756c0b910fa878cc4ebe317b23
João Carlos <joaocarlos.cic1@gmail.com>
2024-07-23 18:04:08 -0300

    delete: deleted jwtservice and messageconfig

--- src/main/java/br/com/fitnessconsultant/service/security/JwtService.java
+++
@@ -1,9 +0,0 @@
-package br.com.fitnessconsultant.service.security;
-
-import br.com.fitnessconsultant.domain.entities.User;
-
-public interface JwtService {
-    String generateToken(User user);
-
-    String getSubjectFromToken(String token);
-}
