c92447eb2262781ac6b43e1b2cf4448e5b0c6664
João Carlos Jr <joaocarlos.cic1@gmail.com>
2025-04-19 16:26:32 -0300

    fix: adicionado limite no campo de telefone

--- src/main/java/br/com/fitnessconsultant/dto/user/RequestUserDTO.java
+++ src/main/java/br/com/fitnessconsultant/dto/user/RequestUserDTO.java
@@ -1,9 +1,16 @@
 package br.com.fitnessconsultant.dto.user;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
+
 public record RequestUserDTO(
         String firstName,
         String lastName,
         String email,
+        @Pattern(regexp = "^[0-9]+$", message = "Phone must contain only digits")
+        @Length(min = 12, max = 12, message = "Número deve conter 12 digitos")
         String phone,
         String password,
         Integer role
