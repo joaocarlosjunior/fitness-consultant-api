ce99b7e8cb7a2cae91f00748c5d0b9fa7d4fcbe4
João Carlos Jr <joaocarlos.cic1@gmail.com>
2025-07-07 15:01:24 -0300

    refactor: alterado tamanho dos campos tipo VARCHAR

--- src/main/resources/db/migration/V1__init_database.sql
+++ src/main/resources/db/migration/V1__init_database.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS tbl_user
     id_user    BIGINT      NOT NULL GENERATED ALWAYS AS IDENTITY,
     first_name VARCHAR(50) NOT NULL,
     last_name  VARCHAR(50) NOT NULL,
-    email      VARCHAR(50) NOT NULL,
+    email      VARCHAR(100) NOT NULL,
     phone      VARCHAR(11) NOT NULL,
     password   VARCHAR,
     is_active  BOOLEAN     NOT NULL,
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS tbl_confirmation_token
 CREATE TABLE IF NOT EXISTS tbl_periodization
 (
     id_periodization BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
-    name             VARCHAR(30),
+    name             VARCHAR(100),
     number_weeks     INTEGER,
     start_date       TIMESTAMP,
     id_user          BIGINT,
@@ -42,7 +42,7 @@ CREATE TABLE IF NOT EXISTS tbl_training
 (
     id_training      BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
     training_type    CHAR,
-    training_name    VARCHAR(20),
+    training_name    VARCHAR(100),
     is_done          BOOLEAN,
     created_at       TIMESTAMP,
     updated_at       TIMESTAMP,
@@ -65,14 +65,14 @@ CREATE TABLE IF NOT EXISTS tbl_training_time
 CREATE TABLE IF NOT EXISTS tbl_muscle_group
 (
     id_muscle_group BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY,
-    name_group      VARCHAR(50),
+    name_group      VARCHAR(100),
     PRIMARY KEY (id_muscle_group)
 );
 
 CREATE TABLE IF NOT EXISTS tbl_exercise_name
 (
     id_exercise_name BIGINT      NOT NULL GENERATED ALWAYS AS IDENTITY,
-    exercise_name    VARCHAR(30) NOT NULL,
+    exercise_name    VARCHAR(100) NOT NULL,
     id_muscle_group  BIGINT,
     CONSTRAINT exercise_name_unique UNIQUE (exercise_name),
     FOREIGN KEY (id_muscle_group) REFERENCES tbl_muscle_group (id_muscle_group) ON DELETE CASCADE,
@@ -86,7 +86,7 @@ CREATE TABLE IF NOT EXISTS tbl_heating
     repetitions      VARCHAR(10),
     initial_load     INTEGER,
     final_load       INTEGER,
-    method_exercise  VARCHAR(20),
+    method_exercise  VARCHAR(100),
     id_training      BIGINT,
     id_exercise_name BIGINT,
     PRIMARY KEY (id_heating),
@@ -101,7 +101,7 @@ CREATE TABLE IF NOT EXISTS tbl_exercise
     repetitions      VARCHAR(10),
     initial_load     INTEGER,
     final_load       INTEGER,
-    method_exercise  VARCHAR(20),
+    method_exercise  VARCHAR(100),
     id_training      BIGINT NULL,
     id_exercise_name BIGINT,
     PRIMARY KEY (id_exercise),
