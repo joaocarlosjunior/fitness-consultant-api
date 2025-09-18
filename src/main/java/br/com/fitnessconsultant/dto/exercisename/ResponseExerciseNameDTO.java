49c7ec5e518bf69bd4c346585568adf1f1ae1548
João Carlos Jr <joaocarlos.cic1@gmail.com>
2025-09-18 09:14:45 -0300

    feat: adicionado anotacao lombok AllArgsConstructor

--- src/main/java/br/com/fitnessconsultant/dto/exercisename/ResponseExerciseNameDTO.java
+++ src/main/java/br/com/fitnessconsultant/dto/exercisename/ResponseExerciseNameDTO.java
@@ -1,10 +1,12 @@
 package br.com.fitnessconsultant.dto.exercisename;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class ResponseExerciseNameDTO {
     private Long idExerciseName;
 
