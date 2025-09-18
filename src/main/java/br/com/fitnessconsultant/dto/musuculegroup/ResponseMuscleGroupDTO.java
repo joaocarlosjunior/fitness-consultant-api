c172f1d4e73463c0c076ccdea3e9a01e3c1dfff3
João Carlos Jr <joaocarlos.cic1@gmail.com>
2025-09-18 14:37:13 -0300

    feat: adicionado anotacao lombok AllArgsConstructor

--- src/main/java/br/com/fitnessconsultant/dto/musuculegroup/ResponseMuscleGroupDTO.java
+++ src/main/java/br/com/fitnessconsultant/dto/musuculegroup/ResponseMuscleGroupDTO.java
@@ -1,10 +1,12 @@
 package br.com.fitnessconsultant.dto.musuculegroup;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class ResponseMuscleGroupDTO {
     private Long idMuscleGroup;
 
