dce09b7f5f04ebc00e0cba6f8aef1c5626bf1856
João Carlos Jr <joaocarlos.cic1@gmail.com>
2025-09-29 17:35:23 -0300

    feat: adiciona constante

--- src/test/java/br/com/fitnessconsultant/common/ExerciseNameConstants.java
+++ src/test/java/br/com/fitnessconsultant/common/ExerciseNameConstants.java
@@ -2,6 +2,7 @@ package br.com.fitnessconsultant.common;
 
 import br.com.fitnessconsultant.domain.entities.ExerciseName;
 import br.com.fitnessconsultant.dto.exercisename.RequestExerciseNameDTO;
+import br.com.fitnessconsultant.dto.exercisename.RequestUpdateExerciseNameDTO;
 import br.com.fitnessconsultant.dto.exercisename.ResponseExerciseNameDTO;
 
 import java.util.Arrays;
@@ -13,7 +14,7 @@ public class ExerciseNameConstants {
     public static final ExerciseName EXERCISE_NAME = new ExerciseName(1L, "Nome do exercicio", MUSCLE_GROUP);
     public static final RequestExerciseNameDTO EXERCISE_NAME_REQUEST = new RequestExerciseNameDTO("Nome exercicio", 1L);
     public static final ResponseExerciseNameDTO EXERCISE_NAME_RESPONSE = new ResponseExerciseNameDTO(1L, "Nome do exercício", "Nome do grupo muscular");
-
+    public static final RequestUpdateExerciseNameDTO EXERCISE_NAME_UPDATE_REQUEST = new RequestUpdateExerciseNameDTO("Nome exercicio atualizado", 1L);
     public static final List<ResponseExerciseNameDTO> EXERCISE_NAME_LIST = Arrays.asList(
             new ResponseExerciseNameDTO(1L, "Nome do exercício 1", "Nome do grupo muscular 1"),
             new ResponseExerciseNameDTO(2L, "Nome do exercício 2", "Nome do grupo muscular 2"),
