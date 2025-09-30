5579185da79775eccab8fc7ad04f7d9e4cff87cf
João Carlos Jr <joaocarlos.cic1@gmail.com>
2025-09-30 12:02:06 -0300

    refactor: seta data atual parametro data

--- src/test/java/br/com/fitnessconsultant/common/PeriodizationConstants.java
+++ src/test/java/br/com/fitnessconsultant/common/PeriodizationConstants.java
@@ -13,11 +13,11 @@ import static br.com.fitnessconsultant.common.UserConstants.USER;
 public class PeriodizationConstants {
     public static final Periodization PERIODIZATION = new Periodization(1L, "Nome Periodizacao", 4, LocalDateTime.now(),LocalDateTime.now(),LocalDateTime.now(), USER);
     public static final RequestPeriodizationDTO PERIODIZATION_REQUEST = new RequestPeriodizationDTO("Nome Periodizacao", 4, 1L);
-    public static final ResponsePeriodizationDTO PERIODIZATION_RESPONSE = new ResponsePeriodizationDTO(1L, "Nome Periodizacao", 4, "2025-09-16T15:42:30", "2025-09-16T15:42:30", "2025-09-16T15:42:30");
+    public static final ResponsePeriodizationDTO PERIODIZATION_RESPONSE = new ResponsePeriodizationDTO(1L, "Nome Periodizacao", 4, LocalDateTime.now().toString(), LocalDateTime.now().toString(), LocalDateTime.now().toString());
     public static final List<ResponsePeriodizationDTO> PERIODIZATION_LIST = Arrays.asList(
-            new ResponsePeriodizationDTO(1L, "Nome Periodizacao 1", 4, "2025-09-16T15:42:30", "2025-09-16T15:42:30", "2025-09-16T15:42:30"),
-            new ResponsePeriodizationDTO(2L, "Nome Periodizacao 2", 4, "2025-09-16T15:42:30", "2025-09-16T15:42:30", "2025-09-16T15:42:30"),
-            new ResponsePeriodizationDTO(3L, "Nome Periodizacao 3", 4, "2025-09-16T15:42:30", "2025-09-16T15:42:30", "2025-09-16T15:42:30"),
-            new ResponsePeriodizationDTO(4L, "Nome Periodizacao 4", 4, "2025-09-16T15:42:30", "2025-09-16T15:42:30", "2025-09-16T15:42:30")
+            new ResponsePeriodizationDTO(1L, "Nome Periodizacao 1", 4, LocalDateTime.now().toString(), LocalDateTime.now().toString(), LocalDateTime.now().toString()),
+            new ResponsePeriodizationDTO(2L, "Nome Periodizacao 2", 4, LocalDateTime.now().toString(), LocalDateTime.now().toString(), LocalDateTime.now().toString()),
+            new ResponsePeriodizationDTO(3L, "Nome Periodizacao 3", 4, LocalDateTime.now().toString(), LocalDateTime.now().toString(), LocalDateTime.now().toString()),
+            new ResponsePeriodizationDTO(4L, "Nome Periodizacao 4", 4, LocalDateTime.now().toString(), LocalDateTime.now().toString(), LocalDateTime.now().toString())
     );
 }
