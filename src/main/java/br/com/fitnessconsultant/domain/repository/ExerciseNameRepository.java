8a9196c65222cf24d7741432c0f60cfcdc22c120
João Carlos Jr <joaocarlos.cic1@gmail.com>
2025-10-06 10:52:13 -0300

    chore: remove importacao nao utilizada

--- src/main/java/br/com/fitnessconsultant/domain/repository/ExerciseNameRepository.java
+++ src/main/java/br/com/fitnessconsultant/domain/repository/ExerciseNameRepository.java
@@ -4,8 +4,6 @@ import br.com.fitnessconsultant.domain.entities.ExerciseName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ExerciseNameRepository extends JpaRepository<ExerciseName, Long> {
     boolean existsByExerciseNameIgnoreCase(String name);
