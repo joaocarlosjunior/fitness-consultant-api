aadbea348d8fad1a3506067ac3c7784472ec8815
João Carlos Jr <joaocarlos.cic1@gmail.com>
2025-10-07 14:54:43 -0300

    refactor: altera length campos repetitions e method

--- src/main/java/br/com/fitnessconsultant/domain/entities/Exercise.java
+++ src/main/java/br/com/fitnessconsultant/domain/entities/Exercise.java
@@ -22,7 +22,7 @@ public class Exercise{
     @Column(name = "series")
     private Integer series;
 
-    @Column(name = "repetitions", length = 10)
+    @Column(name = "repetitions", length = 100)
     private String repetitions;
 
     @Column(name = "initial_load")
@@ -31,7 +31,7 @@ public class Exercise{
     @Column(name = "final_load")
     private Integer finalLoad;
 
-    @Column(name = "method_exercise", length = 20)
+    @Column(name = "method_exercise", length = 100)
     private String method;
 
     @ManyToOne
