44c8dd882399e96a82ea1751e51db1b76649e6bf
João Carlos Jr <joaocarlos.cic1@gmail.com>
2025-10-08 17:48:39 -0300

    test: adiciona sql para testes do MuscleRepository

---
+++ src/test/resources/sql/import_musclegroups.sql
@@ -0,0 +1,4 @@
+INSERT INTO tbl_muscle_group(name_group) VALUES ('Grupo Muscular A');
+INSERT INTO tbl_muscle_group(name_group) VALUES ('Grupo Muscular B');
+INSERT INTO tbl_muscle_group(name_group) VALUES ('Grupo Muscular C');
+INSERT INTO tbl_muscle_group(name_group) VALUES ('Grupo Muscular D');
